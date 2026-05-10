24e21d1cf0dc45f76d412be77229a8bbd47f7c06
mateusribeirog <matribg04@gmail.com>
2026-05-10 00:47:40 -0300

    [chore] merging into main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 
-from evidence_quality import CreateDataRaw, SignalQualityEvaluator
-from pca_from_statistical_analysis import main as run_pca_statistic
-from statistical_analysis import run_statistical_analysis
-from signal_cleaning_validation import ECGSignalCleaner
-from feature_extraction_fft import validation_extraction
-from ica import *
-from wavelet import gerar_features_clinicas
+from src.aquisicao_filtragem.evidence_quality import (
+    CreateDataRaw,
+    SignalQualityEvaluator,
+)
+from src.dimensionality_reduction.pca_from_statistical_analysis import (
+    main as run_pca_statistic,
+)
+from src.aquisicao_filtragem.statistical_analysis import run_statistical_analysis
+from src.aquisicao_filtragem.signal_cleaning_validation import ECGSignalCleaner
+from src.aquisicao_filtragem.feature_extraction_fft import validation_extraction
+from src.aquisicao_filtragem.ica import *
+from src.aquisicao_filtragem.wavelet import gerar_features_clinicas
 
 
 def run_pipeline(
@@ -35,9 +40,14 @@ def run_pipeline(
         shuffle=shuffle,
         to_csv=True,
     )
-    df_raw_filtered = ECGSignalCleaner.clean_signals(df_raw,["I", "II", "III", "AVR", "AVL", "AVF", "V1", "V2", "V3", "V4", "V5", "V6"])
-    df_quality = SignalQualityEvaluator.evaluate_quality(df_raw_filtered, fs=fs, window_sec=window_sec)
-    #df_quality.to_csv(quality_csv, index=False)
+    df_raw_filtered = ECGSignalCleaner.clean_signals(
+        df_raw,
+        ["I", "II", "III", "AVR", "AVL", "AVF", "V1", "V2", "V3", "V4", "V5", "V6"],
+    )
+    df_quality = SignalQualityEvaluator.evaluate_quality(
+        df_raw_filtered, fs=fs, window_sec=window_sec
+    )
+    # df_quality.to_csv(quality_csv, index=False)
     df_filtered_sqi = SignalQualityEvaluator.remove_bad_data(df_quality)
 
     print(f"Raw salvo em: {raw_csv}")
@@ -51,11 +61,11 @@ def run_pipeline(
     )
 
     print("[3/5] Rodando extração de features de energia para segmento")
-    df_features = validation_extraction(df_raw_filtered,df_filtered_sqi)
-    #opcional - remove outliers:
-    #df_features = remove_outliers(df_features)
-    #opcional balanceia pelo menor:
-    #df_features = SignalQualityEvaluator.balancear_classes_undersampling(df_features, max_ratio=2)
+    df_features = validation_extraction(df_raw_filtered, df_filtered_sqi)
+    # opcional - remove outliers:
+    # df_features = remove_outliers(df_features)
+    # opcional balanceia pelo menor:
+    # df_features = SignalQualityEvaluator.balancear_classes_undersampling(df_features, max_ratio=2)
 
     print("[4/5] Rodando extração de features via wavelet")
     df_features_novas = gerar_features_clinicas(df_raw, df_filtered_sqi)
@@ -66,11 +76,25 @@ def run_pipeline(
         output_dir=pca_output_dir,
         save_outputs=True,
     )
-    df_ica_features_stats = ICA(df_features, feature_cols=[col for col in df_features.columns if col not in ["ecg_id","segment_id","label"]])
+    df_ica_features_stats = ICA(
+        df_features,
+        feature_cols=[
+            col
+            for col in df_features.columns
+            if col not in ["ecg_id", "segment_id", "label"]
+        ],
+    )
     df_pca_features_stats = PCA_SIMPLE(df_features, n_components=5)
-    plotar_ica_estatico(df_ica_features_stats,"ICA FEATURE STATS")
+    plotar_ica_estatico(df_ica_features_stats, "ICA FEATURE STATS")
     plotar_ica_estatico(df_pca_features_stats, "PCA FEATURE STATS")
-    df_ica = ICA(df_features_novas, feature_cols=[col for col in df_features_novas.columns if col not in ["ecg_id","segment_id","label"]])
+    df_ica = ICA(
+        df_features_novas,
+        feature_cols=[
+            col
+            for col in df_features_novas.columns
+            if col not in ["ecg_id", "segment_id", "label"]
+        ],
+    )
     df_pca = PCA_SIMPLE(df_features_novas, n_components=5)
     plotar_ica_estatico(df_ica, "ICA FEATURES WAVELET")
     plotar_ica_estatico(df_pca, "PCA FEATURES WAVELET")
